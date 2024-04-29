a18686eae8e4c49efef90b128726d71c07bf40da
SprngVel <kraevvalentin98@gmail.com>
2024-04-29 18:09:43 +1000

    popup window design added

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 //TODO : MAKE search work
-const documentBackground = document.querySelector('html');
-const body = document.querySelector('body');
+// const documentBackground = document.querySelector('html');
+// const body = document.querySelector('body');
+const dialogWindow = document.querySelector('dialog');
 const addBookBtn = document.querySelector('.add-book-btn');
 
-addBookBtn.onclick = function(event) {
-    documentBackground.classList.toggle('darken');
-    event.stopPropagation();
-}
+addBookBtn.addEventListener('click', event => {
+    dialogWindow.showModal();
+});
 
-body.onclick = function() {
-    if(documentBackground.classList.contains('darken')) {
-        documentBackground.classList.remove('darken');
+dialogWindow.addEventListener('click', event => {
+    if (event.target.tagName === 'DIALOG') {
+        event.preventDefault();
+        dialogWindow.close();
     }
-}
+})
+
+// addBookBtn.onclick = function(event) {
+//     documentBackground.classList.toggle('darken');
+//     event.stopPropagation();
+// }
+
+// body.onclick = function() {
+//     if(documentBackground.classList.contains('darken')) {
+//         documentBackground.classList.remove('darken');
+//     }
+// }
+
+
+// stars[0].classList.remove('fa fa-star-o fa-2x');
+// stars[0].classList.add('fa fa-star fa-2x');
 
 /**Add stars for rating? */
 const library = [];
