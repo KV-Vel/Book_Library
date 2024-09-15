c92fbae0ec42f1b5f12fc13a86490cb8ba4a84e0
SprngVel <kraevvalentin98@gmail.com>
2024-09-15 13:00:08 +1000

    local Storage add + code change

--- script.js
+++ script.js
@@ -11,34 +11,71 @@ const searchInput = document.querySelector('#search');
 const searchBtn = document.querySelector('.search-input-icon');
 const [...inputs] = document.querySelectorAll('.book-form > input[type="text"]');
 
-inputs.forEach((input) => input.addEventListener('input', () => {
-  input.reportValidity();
-  if(input.validity.tooShort) {
-    input.setCustomValidity("Value is too short");
-  } else {
-    console.log('ok');
-    input.setCustomValidity("");
-  }
-}));
+function createEmptyMessage () {
+      let emptyMessage = document.createElement("div");
+      emptyMessage.classList.add("empty-message");
+      emptyMessage.textContent = "Your Library is empty";
+      return emptyMessage;
+}
 
-//Display empty library message
-function isLibraryEmpty() {
-  if (!library.length) {
-    emptyMessage = document.createElement("div");
-    emptyMessage.classList.add("empty-message");
-    emptyMessage.textContent = "Your Library is empty";
-
-    libraryWrapper.classList.contains("wrapper-library-center-filled") ?
+class Library {
+  static #books;
+  
+  static isLibraryEmpty () {
+    if (Library.#books.length === 0 || Library.#books.length === null) {
+      
+      const emptyEl = createEmptyMessage()
+      libraryWrapper.classList.contains("wrapper-library-center-filled") ?
       libraryWrapper.classList.replace("wrapper-library-center-filled", "wrapper-library-center-empty") :
       libraryWrapper.classList.add("wrapper-library-center-empty");
+      libraryWrapper.append(emptyEl);
+      return true
+    } else {
+      libraryWrapper.classList.replace("wrapper-library-center-empty", "wrapper-library-center-filled");
 
-    libraryWrapper.append(emptyMessage);
-  } else {
-    libraryWrapper.classList.replace("wrapper-library-center-empty", "wrapper-library-center-filled");
-    emptyMessage.remove();
+      if (document.querySelector('.empty-message')) document.querySelector('.empty-message').remove();
+      return false
+    }
+  }
+
+  constructor () {
+    localStorage.getItem('library') === null ? Library.#books = [] : Library.#books = JSON.parse(localStorage.getItem('library'));
+    if(Library.#books.length !== 0) {
+      libraryWrapper.className = "wrapper-library-center wrapper-library-center-filled";
+      displayBooks(JSON.parse(localStorage.getItem('library')));
+    } else {
+      Library.isLibraryEmpty();
+    }
+  }
+
+  get books () {
+    return Library.#books;
+  }
+
+  addBookToLibrary() {
+    const titleInput = document.querySelector(".titleInput").value;
+    const authorInput = document.querySelector(".authorInput").value;
+    const pagesInput = document.querySelector(".pagesInput").value;
+    const statusInput = document.querySelector(".statusInput").checked;
+    
+    Library.#books.push(new Book(titleInput, authorInput, pagesInput, statusInput));
+    Library.isLibraryEmpty();
+
+    localStorage.setItem('library', JSON.stringify(Library.#books));
+  }
+
+  removeBook(id) {
+    Library.#books = Library.#books.filter((book) => book.id !== id);
+    localStorage.setItem('library', JSON.stringify(Library.#books));
+    Library.isLibraryEmpty();
+  }
+
+  changeStatus(id, status) {
+    const foundBook = Library.#books.find((book) => book.id === id);
+    foundBook.status = status;
+    localStorage.setItem('library', JSON.stringify(Library.#books));
   }
 }
-isLibraryEmpty(); // ON DOM LOAD?
 
 class Book {
   constructor (title, author, pages, status) {
@@ -46,36 +83,63 @@ class Book {
     this.author = author;
     this.pages = pages;
     this.status = status;
+    this.id = Date.now().toString();
   }
 }
-
-function addBookToLibrary() {
-  const titleInput = document.querySelector(".titleInput").value;
-  const authorInput = document.querySelector(".authorInput").value;
-  const pagesInput = document.querySelector(".pagesInput").value;
-  const statusInput = document.querySelector(".statusInput").checked;
+const lib = new Library();
   
-  library.push(new Book(titleInput, authorInput, pagesInput, statusInput));
+  inputs.forEach((input) => input.addEventListener('input', () => { checkValidity(input) }));
+  addBookBtn.addEventListener("click", showDialogWindow);
+  dialogWindow.addEventListener("click", closeDialog);
+  formInputs.forEach((input) => {
+    input.addEventListener('focus', () => { applyUnderline(input) });
+    input.addEventListener('blur', () => { removeUnderline(input) });
+  });
+  
 
-  form.reset();
-}
-
-function checkValidity(input) {
-  console.log(input.validity.tooShort)
-  if(input.validity.tooShort) {
-    input.setCustomValidity("Value is too short");
-    return;
-  } else {
-    input.setCustomValidity("");
+  function checkValidity(input) {
+    input.reportValidity();
+    if(input.validity.tooShort) {
+      input.setCustomValidity("Value is too short");
+    } else {
+      input.setCustomValidity("");
+    }
   }
-}
 
-function displayNewBook() {
-  let lastAddedBookInLibrary = library.at(-1);
+  function closeDialog(event) {
+    if (event.target.tagName === "DIALOG") {
+      event.preventDefault();
+      dialogWindow.classList.add('hide-dialog');
+  
+      setTimeout(() => {
+        dialogWindow.classList.remove('hide-dialog');
+        dialogWindow.close();
+      }, 500)
+    }
+  }
+  
+  function showDialogWindow() {
+    dialogWindow.showModal();
+    dialogWindow.classList.add('show-dialog');
+  
+    setTimeout(() => {
+      dialogWindow.classList.remove('show-dialog');
+    }, 1300)
+  }
+
+  function applyUnderline(input) {
+    input.previousElementSibling.style.cssText = 'background-size: 0 1px, 100% 1px';
+  }
+
+  function removeUnderline(input) {
+    input.previousElementSibling.style.cssText = 'background-size: 100% 1px, 0 1px';
+  }
+
+  function createBookUI(book) {
   // DIV - Book wrapper
   const divBook = document.createElement("div");
   divBook.classList.add("book");
-  divBook.setAttribute("data-number", String(library.length - 1));
+  divBook.setAttribute("data-number", book.id);
   libraryWrapper.append(divBook);
   // FRONT COVER
   const frontCoverBook = document.createElement("div");
@@ -87,11 +151,11 @@ function displayNewBook() {
   // BOOK PAGES 
   const bookPages = document.createElement("h6");
   bookPages.classList.add("additional-book-info");
-  bookPages.textContent = `${lastAddedBookInLibrary.pages} pag.`;
+  bookPages.textContent = `${book.pages} pag.`;
   // BOOK STATUS
   const bookStatus = document.createElement("button");
   bookStatus.classList.add("additional-book-info-status");
-  if (!lastAddedBookInLibrary.status) {
+  if (!book.status) {
     bookStatus.textContent = "Unread";
     bookStatus.classList.add("unread");
   } else {
@@ -101,125 +165,95 @@ function displayNewBook() {
 
   frontCoverBook.append(headerBook, bookPages, bookStatus);
   // Changing book status
-  bookStatus.addEventListener("click", () => {
-    if (bookStatus.classList.contains("unread") || bookStatus.classList.contains("read")) {
+  bookStatus.addEventListener("click", (e) => { changeBookStatusUI(e) });
 
-      bookStatus.classList.contains("unread") ?
-        bookStatus.classList.replace("unread", "read") :
-        bookStatus.classList.replace("read", "unread");
-  
-      bookStatus.classList.add("shrink-btn");
+//BOOK TITLE
+const headerBookTitle = document.createElement("h4");
+headerBookTitle.textContent = book.title;
+// BOOK AUTHOR
+const headerBookAuthor = document.createElement("h5");
+headerBookAuthor.textContent = book.author;
+headerBook.append(headerBookTitle, headerBookAuthor);
+// DELETE BUTTON WRAPPER
+const deleteBtnWrapper = document.createElement("div");
+deleteBtnWrapper.classList.add("delete-btn-wrapper");
+frontCoverBook.append(deleteBtnWrapper);
+// DELETE BUTTON 
+const deleteBtn = document.createElement("button");
+deleteBtnWrapper.append(deleteBtn);
+// DELETE ICON
+const deleteIcon = document.createElement("img");
+deleteIcon.setAttribute("src", "./sources/icons8-delete-30.png");
+deleteIcon.width = 15;
+deleteIcon.height = 14;
+deleteBtn.append(deleteIcon);
+
+//Delete book button 
+deleteBtn.addEventListener("click", (e) => { deleteBookUI(e) });
+  }
+
+  function displayBooks(books) {
+    const allBooksUI = document.querySelectorAll('.book');
+    allBooksUI.forEach((book) => book.remove());
+    books.forEach((book) => createBookUI(book));
+  }
+
+  function deleteBookUI(e) {
+    const book = e.target.closest('.book');
+    const bookID = book.getAttribute('data-number');
+    lib.removeBook(bookID);
+    book.remove();
+    displayBooks(lib.books);
+  }
+
+  function changeBookStatusUI(e) {
+    if (e.target.classList.contains("unread") || e.target.classList.contains("read")) {
+
+      e.target.classList.contains("unread") ?
+      e.target.classList.replace("unread", "read") :
+      e.target.classList.replace("read", "unread");
+
+      e.target.classList.add("shrink-btn");
       setTimeout(() => {
-        bookStatus.classList.remove("shrink-btn");
-  
-        bookStatus.classList.contains("unread") ?
-          bookStatus.textContent = "Unread" :
-          bookStatus.textContent = "Read";
-  
+        e.target.classList.remove("shrink-btn");
+
+        e.target.classList.contains("unread") ?
+          e.target.textContent = "Unread" :
+          e.target.textContent = "Read";
+
       }, 400);
-      if (bookStatus.classList.contains("shrink-btn")) {
-        bookStatus.textContent = "";
+      if (e.target.classList.contains("shrink-btn")) {
+        e.target.textContent = "";
       }
     }
-  });
+    e.target.classList.contains('unread') ? lib.changeStatus(e.target.closest('.book').getAttribute('data-number'), 'unread') : lib.changeStatus(e.target.closest('.book').getAttribute('data-number'), 'read');
+  }
 
-  //BOOK TITLE
-  const headerBookTitle = document.createElement("h4");
-  headerBookTitle.textContent = lastAddedBookInLibrary.title;
-  // BOOK AUTHOR
-  const headerBookAuthor = document.createElement("h5");
-  headerBookAuthor.textContent = lastAddedBookInLibrary.author;
-  headerBook.append(headerBookTitle, headerBookAuthor);
-  // DELETE BUTTON WRAPPER
-  const deleteBtnWrapper = document.createElement("div");
-  deleteBtnWrapper.classList.add("delete-btn-wrapper");
-  frontCoverBook.append(deleteBtnWrapper);
- // DELETE BUTTON 
-  const deleteBtn = document.createElement("button");
-  deleteBtnWrapper.append(deleteBtn);
-  // DELETE ICON
-  const deleteIcon = document.createElement("img");
-  deleteIcon.setAttribute("src", "./sources/icons8-delete-30.png");
-  deleteIcon.width = 15;
-  deleteIcon.height = 14;
-  deleteBtn.append(deleteIcon);
-
-  //Delete book button 
-  deleteBtn.addEventListener("click", () => {
-    const dataNumber = divBook.getAttribute("data-number");
-    // Reduce data-number if book was deleted
-    allBooks = document.querySelectorAll('.book');
-  
-    allBooks.forEach((book) => {
-      if (dataNumber < book.dataset.number) {
-        book.dataset.number = book.dataset.number - 1;
-      } 
-    });
-  
-    library.splice(dataNumber, 1);
-    divBook.remove();
-    isLibraryEmpty();
-  });
-  
-}
-  //add book show dialog window
-  addBookBtn.addEventListener("click", showDialogWindow);
-
-  //close onclick outside of dialog
-  dialogWindow.addEventListener("click", closeDialog);
+  function findClass(e, className) {
+    const elArr = Array.from(e.target.classList);
+    return elArr.find(className);
+  }
 
 /**EVENT LISTENERS */
-  
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  addBookToLibrary();
-  isLibraryEmpty();
-  displayNewBook();
+  
+  lib.addBookToLibrary();
+  displayBooks(lib.books);
+  
   dialogWindow.close();
+  form.reset();
 });
 
-formInputs.forEach((input) => {
-  input.addEventListener('focus', () => {
-    // Styling labels on input focus
-    input.previousElementSibling.style.cssText = 'background-size: 0 1px, 100% 1px';
-  });
-});
-
-formInputs.forEach((input) => {
-  input.addEventListener('blur', () => {
-    input.previousElementSibling.style.cssText = 'background-size: 100% 1px, 0 1px';
-  });
-});
-
+// ПОЗЖЕ РАЗОБРАТЬСЯ
 searchInput.addEventListener('search', findBook);
 searchBtn.addEventListener('click', findBook);
-
-function closeDialog(event) {
-  if (event.target.tagName === "DIALOG") {
-    event.preventDefault();
-    dialogWindow.classList.add('hide-dialog');
-
-    setTimeout(() => {
-      dialogWindow.classList.remove('hide-dialog');
-      dialogWindow.close();
-    }, 500)
-  }
-}
-
-function showDialogWindow() {
-  dialogWindow.showModal();
-  dialogWindow.classList.add('show-dialog');
-
-  setTimeout(() => {
-    dialogWindow.classList.remove('show-dialog');
-  }, 1300)
-}
 
 // Search for book using input
 function findBook() {
   allBooks = document.querySelectorAll('.book');
   const userInput = searchInput.value;
-  // Taking index of Books that match search
+  
   const resultOfUserSearch = library.map((book, index) => {if (book.title.includes(userInput) || book.author.includes(userInput)) {return index}})
                                     .filter(bookIndex => bookIndex !== undefined);
 
